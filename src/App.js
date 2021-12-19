e1434e7ba0dd2edf281ef5ce174e801d3e803335
JLiao <jian.liao1@ucalgary.ca>
2021-12-18 21:57:24 -0700

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+    Flex,
+    FormControl, FormLabel,
+    Select,
+} from "@chakra-ui/react";
 import './App.css';
+import './utils/language'
+import {useState} from "react";
+
+let final_transcript = '';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition
+    let recognition = new SpeechRecognition()
+
+    recognition.lang = 'en-US'
+    recognition.continuous = true;
+    recognition.interimResults = true;
+
+    const [transcript, setTranscript] = useState("Listening...")
+
+    const renderSpeech = () => {
+        recognition.start()
+    }
+
+    recognition.onresult = (event) => {
+        let interim_transcript = "";
+
+        for (let i = event.resultIndex; i < event.results.length; ++i) {
+            if (event.results[i].isFinal) {
+                final_transcript += event.results[i][0].transcript;
+            } else {
+                interim_transcript += event.results[i][0].transcript;
+            }
+
+            setTranscript(interim_transcript)
+        }
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Flex
+                    direction="column"
+                    minH="100vh"
+                    align="center"
+                    justify={"center"}
+                    pt={{sm: "120px", md: "75px"}}
+                >
+                    <button onClick={renderSpeech}>Press to Talk</button>
+
+                    {/*<span id="final" className="text-black">transcript</span>*/}
+                    <span id="interim" className="text-secondary">{transcript}</span>
+
+
+                </Flex>
+            </header>
+        </div>
+    );
 }
 
 export default App;
